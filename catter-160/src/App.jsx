d6519cff1b611101c388a216dd75901d2222ba08
NakVong <kiriratanakvong@berkeley.edu>
2025-08-01 16:54:18 -0700

    added shadcn and lucide for icons

--- catter-160/src/App.jsx
+++ catter-160/src/App.jsx
@@ -4,6 +4,8 @@ import MainPage from './pages/MainPage';
 import GuidePage from './pages/GuidePage';
 import ChatPage from './pages/ChatPage';
 import ProfilePage from './pages/ProfilePage';
+import { Button } from "@/components/ui/button"
+import { Mail, ArrowRight } from "lucide-react"
 import './App.css';
 
 function App() {
@@ -34,14 +36,23 @@ function App() {
   //   </>
   // )
   return (
-    <Router>
-      <Routes>
-        <Route path="/main" element={ <MainPage/> }/>
-        <Route path="/guide" element={ <GuidePage/> }/>
-        <Route path="/chat" element={ <ChatPage/> }/>
-        <Route path="/profile" element={ <ProfilePage/> }/>
-      </Routes>
-    </Router>
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>
+          <Mail className="mr-2 h-4 w-4" />
+          Send Mail
+          <ArrowRight className="ml-2 h-4 w-4" />
+        </Button>
+      </div>
+      <Router>
+        <Routes>
+          <Route path="/main" element={ <MainPage/> }/>
+          <Route path="/guide" element={ <GuidePage/> }/>
+          <Route path="/chat" element={ <ChatPage/> }/>
+          <Route path="/profile" element={ <ProfilePage/> }/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
