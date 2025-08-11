f12dcf97985ec8d35e5523a9b060593444e72d67
NakVong <kiriratanakvong@berkeley.edu>
2025-08-11 14:23:49 -0700

    updated header component and moved profile button

--- catter-160/src/pages/MainPage.jsx
+++ catter-160/src/pages/MainPage.jsx
@@ -1,14 +1,13 @@
 import NavBar from "@/components/NavBar";
 import CatSwiper from "./CatSwiper";
-import logo from "../assets/Catter.png";
-import paw from "../assets/catter-logo.png";
+import HeaderBar from "@/components/HeaderBar";
 
 const MainPage = () => {
 	return (
 		<div>
 			{/* Logo in top-left */}
-			<img src={paw} alt="paw" className="h-25 w-auto mt-2 -ml-12" />
-
+			<HeaderBar/>
+		
 			<CatSwiper />
 			<NavBar />
 		</div>
